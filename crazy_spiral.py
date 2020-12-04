9a2bb640dc3d7d59a2b5b5ae2c494964713a08bb
unknown <>
2020-12-04 15:23:01 +0100

    Update crazy_spiral.py

--- crazy_spiral.py
+++ crazy_spiral.py
@@ -1,1 +1,19 @@
 import turtle as t
+from itertools import cycle
+
+colors = cycle(['yellow', 'green', 'blue', 'purple'])
+
+def draw_circle(size, angle, shift):
+    t.pencolor(next(colors))
+    t.circle(size)
+    t.right(angle)
+    t.forward(shift)
+    #ending condition: size of cirle < 120
+    if size < 120:
+        draw_circle(size+5, angle + 2, shift + 3)
+
+t.bgcolor('black')
+t.speed('fast')
+t.pensize(4)
+
+draw_circle(30, 0, 1)
